c37064097f1985bbc490e0397fdac54036a6f9c2
junbin <ghdwnsqls0@naver.com>
2025-05-12 15:33:33 +0900

    feat: Controller 권한 테스트 api 구현

--- src/main/java/com/hong/springauthapp/user/UserController.java
+++ src/main/java/com/hong/springauthapp/user/UserController.java
@@ -3,7 +3,9 @@ package com.hong.springauthapp.user;
 import com.hong.springauthapp.common.HttpResponseDto;
 import com.hong.springauthapp.user.dto.SignupRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import static com.hong.springauthapp.common.ResponseEnum.USER_SIGNUP_SUCCESS;
@@ -20,6 +22,18 @@ public class UserController {
     public ResponseEntity<HttpResponseDto> signup(@RequestBody SignupRequestDto requestDto) {
         userService.signup(requestDto);
         return of(USER_SIGNUP_SUCCESS);
+    }
+
+    @GetMapping("/user-role")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public ResponseEntity<String> userResponseTest() {
+        return ResponseEntity.status(HttpStatus.OK).body("유저 권한 테스트");
+    }
+
+    @GetMapping("/admin-role")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<String> adminResponseTest() {
+        return ResponseEntity.status(HttpStatus.OK).body("어드민 권한 테스트");
     }
 
 }
