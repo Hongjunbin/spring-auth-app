f0f8905a817441b9c6f76f30715612edc6a941e5
junbin <ghdwnsqls0@naver.com>
2025-07-24 16:19:52 +0900

    update : Controller API swagger description add

--- src/main/java/com/hong/springauthapp/user/controller/AuthController.java
+++ src/main/java/com/hong/springauthapp/user/controller/AuthController.java
@@ -5,12 +5,15 @@ import com.hong.springauthapp.common.ResponseEnum;
 import com.hong.springauthapp.user.dto.EmailRequest;
 import com.hong.springauthapp.user.dto.VerificationRequest;
 import com.hong.springauthapp.user.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.hong.springauthapp.common.ResponseUtils.of;
 
+@Tag(name = "인증 API", description = "이메일 전송 및 인증 추후의 다른 인증 추가예정")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -19,12 +22,14 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/email/send")
+    @Operation(summary = "이메일 인증 코드 발급 및 이메일 전송")
     public ResponseEntity<HttpResponse> sendVerificationEmail(@RequestBody EmailRequest request) {
         authService.sendVerificationEmail(request.email());
         return of(ResponseEnum.USER_SIGNUP_SUCCESS);
     }
 
     @PostMapping("/email/verify")
+    @Operation(summary = "이메일 인증 코드 검증 및 인증 완료 상태 저장")
     public ResponseEntity<HttpResponse> verifyEmailCode(@RequestBody VerificationRequest request) {
         authService.verifyEmailCode(request);
         return of(ResponseEnum.USER_SIGNUP_SUCCESS);
--- src/main/java/com/hong/springauthapp/user/controller/UserController.java
+++ src/main/java/com/hong/springauthapp/user/controller/UserController.java
@@ -3,6 +3,9 @@ package com.hong.springauthapp.user.controller;
 import com.hong.springauthapp.common.HttpResponse;
 import com.hong.springauthapp.user.UserService;
 import com.hong.springauthapp.user.dto.SignupRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,14 +14,16 @@ import org.springframework.web.bind.annotation.*;
 import static com.hong.springauthapp.common.ResponseEnum.*;
 import static com.hong.springauthapp.common.ResponseUtils.of;
 
+@Tag(name = "유저 API", description = "회원가입, 권한 테스트")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-
+    
     @PostMapping("/signup")
+    @Operation(summary = "회원가입: 이메일 인증 후 인증 완료 상태가 있어야 가능")
     public ResponseEntity<HttpResponse> signup(@RequestBody SignupRequest request) {
         userService.signup(request);
         return of(USER_SIGNUP_SUCCESS);
@@ -26,12 +31,14 @@ public class UserController {
 
     @GetMapping("/user-role")
     @PreAuthorize("hasAuthority('ROLE_USER')")
+    @Operation(summary = "일반 유저 권한 테스트")
     public ResponseEntity<HttpResponse> userResponseTest() {
         return of(USER_AUTHORITY_TEST);
     }
 
     @GetMapping("/admin-role")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @Operation(summary = "어드민 유저 권한 테스트")
     public ResponseEntity<HttpResponse> adminResponseTest() {
         return of(ADMIN_AUTHORITY_TEST);
     }
